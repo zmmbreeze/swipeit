4d970432c7d7a6d415e2201b8263c5a419bf7585
zmmbreeze <zmmbreeze0825@gmail.com>
2015-06-15 18:23:34 +0800

    Updated

--- src/swipeit.js
+++ src/swipeit.js
@@ -1,4 +1,24 @@
 (function (win) {
+    // requestAnimationFrame
+    // https://github.com/facebook/react/blob/38acadf6f493926383aec0362617b8507ddee0d8/src/shared/vendor/core/requestAnimationFrame.js
+    var lastTime = 0;
+    var requestAnimationFrame =
+        window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function (callback) {
+            var currTime = Date.now();
+            var timeDelay = Math.max(0, 16 - (currTime - lastTime));
+            lastTime = currTime + timeDelay;
+            return global.setTimeout(function () {
+                callback(Date.now());
+            }, timeDelay);
+        };
+
+    // Works around a rare bug in Safari 6 where the first request is never invoked.
+    requestAnimationFrame(function () {});
 
     var Swipe = function (element) {
         /**
@@ -10,6 +30,11 @@
          * @type {Object}
          */
         this._startPos;
+
+        /**
+         * @type {boolean}
+         */
+        this._animating = false;
     };
 
     /**
@@ -76,11 +101,13 @@
             'touchmove': this._onmove,
             'touchend': this._onend,
             'touchcancel': this._onend,
-            'mousemove', this._onmove,
-            'mouseup', this._onend
+            'mousemove': this._onmove,
+            'mouseup': this._onend
         });
 
         this._startPos = this._getPos(evt);
+        this.e.style.WebkitTransition = 'initial';
+        this.e.style.transition = 'initial';
     };
 
     /**
@@ -98,8 +125,8 @@
             'touchmove': this._onmove,
             'touchend': this._onend,
             'touchcancel': this._onend,
-            'mousemove', this._onmove,
-            'mouseup', this._onend
+            'mousemove': this._onmove,
+            'mouseup': this._onend
         });
     };
 
@@ -108,7 +135,24 @@
      * @param {TouchEvent} evt
      */
     Swipe.prototype._onmove = function (evt) {
+        var point = this._getPos(evt);
 
+        if (this._animating) {
+            return;
+        }
+
+        this._animating = true;
+        var that = this;
+        requestAnimationFrame(function (time) {
+            if (!that._animating) {
+                return;
+            }
+
+            // TODO
+            console.log(point);
+
+            that._animating = false;
+        });
     };
 
     window.swipeit = function (element) {
